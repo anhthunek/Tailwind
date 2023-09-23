7f0b779533614c3bab7ccafe0440910538e87684
anhthunek <imcatee0@gmail.com>
2023-09-23 22:24:42 +0700

    config, jit, res

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Just-in-time mode: đỡ config nhiều, []
+  mode: "jit",
   content: ["./public/**/*.{html,js}"],
+
+  // config spacing, colors
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        35: "35px",
+      },
+      colors: {
+        "gray#333": "#333",
+      },
+    },
   },
   plugins: [],
-}
-
+};
